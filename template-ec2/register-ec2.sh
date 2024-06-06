479951c3ebba971ffdc59ee9afb4d6abf5af3969
Lucas Andrade de Oliveira <lucas.oliveira@irede.org.br>
2024-06-06 08:32:18 -0300

    change script registre and template.tf

--- template-ec2/register-ec2.sh
+++ template-ec2/register-ec2.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-apt update -y
-curl -O https://s3.us-east-1.amazonaws.com/amazon-ecs-agent-us-east-1/amazon-ecs-init-latest.amd64.deb
-apt --fix-broken install -y
-dpkg -i amazon-ecs-init-latest.amd64.deb
-systemctl start docker.service
-su <<EOF echo ECS_CLUSTER=cluster-demo >> /etc/ecs/ecs.config 
-EOF
-systemctl start ecs
+sudo apt update -y
+sudo curl -O https://s3.us-east-1.amazonaws.com/amazon-ecs-agent-us-east-1/amazon-ecs-init-latest.amd64.deb
+sudo apt --fix-broken install -y
+sudo dpkg -i amazon-ecs-init-latest.amd64.deb
+sudo systemctl start docker.service
+echo ECS_CLUSTER=cluster-demo  | sudo tee /etc/ecs/ecs.config 
+sudo systemctl start ecs
